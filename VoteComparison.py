558febb916703a4a90fdfc379d96a39ffeb65d90
krause-matt <76706251+krause-matt@users.noreply.github.com>
2021-01-03 11:24:10 -0600

    Update VoteComparison.py
    Testing new functions such as describing vote title where differences occurred.

--- VoteComparison.py
+++ VoteComparison.py
@@ -23,3 +23,55 @@ for i in range(1,314):
             print("Vote number : " + str(i) + " -- SANDERS = " + sanders_vote.upper() + ", WARREN = " + warren_vote.upper())
         else:
             print("Vote number : " + str(i) + " -- someone not voting.")
+            
+"""
+for i in range(1,20):
+    json_response = requests.get(url + str(i) + ".json", headers = headers)
+    data = json.loads(json_response.text)
+    for x in data["results"]["votes"]["vote"]["bill"]:
+        if x == "title":
+            print(i)
+            for y in data["results"]["votes"]["vote"]["bill"]["title"]:
+                print(y, end="")
+
+"""
+"""
+for i in range(3,4):
+    json_response = requests.get(url + str(i) + ".json", headers = headers)
+    data = json.loads(json_response.text)
+    sponsor = ""
+    description = ""
+    for x in data["results"]["votes"]["vote"]["amendment"]["sponsor"]:
+        sponsor = sponsor + x
+    for x in data["results"]["votes"]["vote"]["description"]:
+        description = description + x
+    print(sponsor)
+    print(description)
+"""
+"""
+for i in range(1,340):
+    json_response = requests.get(url + str(i) + ".json", headers = headers)
+    data = json.loads(json_response.text)
+    sponsor = ""
+    description = ""
+    print(str(i))
+    if not data["results"]["votes"]["vote"]["amendment"]:
+        pass
+    else:
+        if not data["results"]["votes"]["vote"]["amendment"]["sponsor"]:
+            pass
+        else:
+            for y in data["results"]["votes"]["vote"]["amendment"]["sponsor"]:
+                sponsor = sponsor + y
+    if sponsor == "Bernard Sanders":
+        for z in data["results"]["votes"]["vote"]["description"]:
+            description = description + z
+        print("Vote " + str(i) + " : Bernie Amendment = " + description)
+
+"""
+"""
+for i in range(243,244):
+    json_response = requests.get(url + str(i) + ".json", headers = headers)
+    data = json.loads(json_response.text)
+    print(data)
+"""
